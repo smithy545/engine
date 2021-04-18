7adb4680a4103624349a1e21b116dc537d1d1110
Philip Smith <smithy545@gmail.com>
2021-04-18 02:27:47 -0500

    Initial commit

---
+++ include/engine/Scene.h
@@ -0,0 +1,43 @@
+//
+// Created by Philip Smith on 10/17/2020.
+//
+
+#ifndef CIVILWAR_SCENE_H
+#define CIVILWAR_SCENE_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <gl/glew.h>
+#include <string>
+#include <vector>
+
+
+struct Scene {
+    struct Mesh {
+        std::vector<glm::vec3> vertices;
+        std::vector<glm::vec3> colors;
+        std::vector<unsigned int> indices;
+    };
+    struct InstanceList {
+        Mesh mesh;
+        std::string name;
+        std::vector<glm::mat4> instances;
+        enum RenderStrategy {
+            POINTS,
+            LINES,
+            TRIANGLES
+        } render_strategy{TRIANGLES};
+    };
+    struct Light {
+        glm::vec3 position;
+        double radius;
+    };
+    struct DirectedLight : Light {
+        glm::vec3 direction;
+    };
+    std::vector<Light *> lights;
+    std::vector<InstanceList> instances;
+};
+
+
+#endif //CIVILWAR_SCENE_H
